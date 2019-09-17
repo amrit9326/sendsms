f3e2a5416c6a92a3c1f64d2ecfba235838490ab8
Amrit Singh <38880261+amrit9326@users.noreply.github.com>
2019-09-18 01:42:42 +0530

    Update textmsg.py

--- textmsg.py
+++ textmsg.py
@@ -1,11 +1,11 @@
 import requests
 import json
 
-URL = 'https://www.way2sms.com/api/v1/sendCampaign'
+URL = ''
 
 # get request
-apiKey="1ZBYLLHWBUQ11QFMJUD4F3GPL4HZ1LLC"
-secretKey="F6F30X3EV6WO9854"
+apiKey=""
+secretKey=""
 def sendPostRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textMessage):
   req_params = {
   'apikey':apiKey,
@@ -22,12 +22,5 @@ number=input("Enter Number:")
 while True:
 	
 	s=input("Enter Message :")
-	response =sendPostRequest(URL, apiKey,secretKey, 'stage', number, '9125183799',s)
+	response =sendPostRequest(URL, apiKey,secretKey, 'stage', number, 'senderid',s)
 	print (response.text)
-"""
-  Note:-
-    you must provide apikey, secretkey, usetype, mobile, senderid and message values
-    and then requst to api
-"""
-# print response if you want
-    
